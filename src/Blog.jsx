9fa4dfdc93d147292cfbb70f281d47185eb80ac2
Daisuke <dai010830@gmail.com>
2020-07-06 22:34:53 +0900

    React#8

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,61 +3,73 @@ import Article from './Article';
 import * as Foobar from './components/Foobar'; //ここは相対パス
 import Hoge from './components/hoge';
 
-class Blog extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count:0
-    }
-  }
 
-  componentDidMount() {
-    //ボタンクリックされたらいいねをカウントアップする
-  document.getElementById('counter').addEventListener('click',this.countUp)
+// class Blog extends React.Component{
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isPublished: false,
+//       count:0
+//     }
+//   }
+
+const Blog = () => {
+
+  return (
+    <>
+      <Article
+        title={"Reactの使い方"}
+      // count={this.state.count}
+      />
+      <Foobar.Foo />
+      <Foobar.Bar />
+      <Hoge />
+    </>
+  )
+
 }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState({count:0})
-    }
-  }
 
-  componentWillUnmount() {
-    document.getElementById('counter').removeEventListener('click',this.countUp)
-  }
+//   componentDidMount() {
+//     //ボタンクリックされたらいいねをカウントアップする
+//   document.getElementById('counter').addEventListener('click',this.countUp)
+// }
+
+//   componentDidUpdate() {
+//     if (this.state.count >= 10) {
+//       this.setState({count:0})
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     document.getElementById('counter').removeEventListener('click',this.countUp)
+//   }
 
   //公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
+
   
-  countUp = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  };
+  // countUp = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // };
 
-  render() {
-    const authorName = 'torahack';
-    return (
-      <>
-        <Article
-          title={"Reactの使い方"}
-          isPublished={this.state.isPublished}
-          toggle={() => { this.togglePublished() }}
-          count={this.state.count}
-          countUp ={()=>{this.countUp()}}
-        />
-        <Foobar.Foo />
-        <Foobar.Bar />
-        <Hoge />
-      </>
-    )
-  }
 
-}
+const authorName = 'torahack';
+    
+    // return (
+    //   <>
+    //     <Article
+    //       title={"Reactの使い方"}
+    //       // count={this.state.count}
+          
+    //     />
+    //     <Foobar.Foo />
+    //     <Foobar.Bar />
+    //     <Hoge />
+    //   </>
+    // )
+  
+
 
 export default Blog
